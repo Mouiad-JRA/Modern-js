813d2c682ea3521be41a2acd373413a0ab63d099
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-20 21:51:02 +0300

    WIP : Part one, section 2

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+// let a = 1, b = 1;
+//
+// let c = ++a; // 2
+// let d = b++; // 1
+
+// let a = 2;
+//
+// let x = 1 + (a *= 2); // 5
+// Fix the addition
+// let a = prompt("First number?", 1);
+// let b = prompt("Second number?", 2);
+
+// alert(a + b); // 12
+// -->
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
+
+alert(a + b); // 3
