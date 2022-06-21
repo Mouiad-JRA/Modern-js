049ef9d8b66011f7d5de6b0b3f3b3e40e3ae465d
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-21 11:10:02 +0300

    WIP : Part one, ifelse

--- script.js
+++ script.js
@@ -27,3 +27,63 @@
 // undefined === null // False
 // null == "\n0\n" // False
 // null === +"\n0\n" // False
+
+
+// Will alert be shown?
+// if ("0") {
+//     alert( 'Hello' );
+// } //hello
+
+/*
+
+Using the if..else construct, write the code which asks: ‘What is the “official” name of JavaScript?’
+
+If the visitor enters “ECMAScript”, then output “Right!”, otherwise – output: “You don’t know? ECMAScript!”
+ */
+
+// const coders = prompt('What is the “official” name of JavaScript?');
+// if (coders == 'ECMAScript')
+//     alert("Right!")
+// else
+//     alert("You don’t know? ECMAScript!")
+
+// Using if..else, write the code which gets a number via prompt and then shows in alert:
+//
+// 1, if the value is greater than zero,
+// -1, if less than zero,
+// 0, if equals zero.
+// const value = Numnber(prompt('Enter a number '));
+// if (value > 0)
+//     alert("1")
+// else if (value < 0)
+//     alert("-1")
+// else
+//     alert(0)
+
+// Rewrite this if using the conditional operator '?':
+//let result;
+//
+// if (a + b < 4) {
+//   result = 'Below';
+// } else {
+//   result = 'Over';
+// }
+// let a , b = 1;
+// let result = (a+b < 4) ? 'Below' : 'Over';
+//Rewrite if..else using multiple ternary operators '?'.
+//
+// For readability, it’s recommended to split the code into multiple lines.
+// let message;
+//
+// if (login == 'Employee') {
+//     message = 'Hello';
+// } else if (login == 'Director') {
+//     message = 'Greetings';
+// } else if (login == '') {
+//     message = 'No login';
+// } else {
+//     message = '';
+// }
+
+let messages;
+messages = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' :(login == '') ? 'No login': '';
