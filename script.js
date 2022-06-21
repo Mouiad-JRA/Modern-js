c41b00d51bdfe4ed8ac6ba37e7d9b39a1e427967
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-21 10:40:01 +0300

    WIP : Part one, comparison

--- script.js
+++ script.js
@@ -12,8 +12,18 @@
 // let b = prompt("Second number?", 2);
 
 // alert(a + b); // 12
-// -->
-let a = +prompt("First number?", 1);
-let b = +prompt("Second number?", 2);
+// // -->
+// let a = +prompt("First number?", 1);
+// let b = +prompt("Second number?", 2);
+//
+// alert(a + b); // 3
 
-alert(a + b); // 3
+// What will be the result for these expressions?
+
+// 5 > 4 // True
+// "apple" > "pineapple" // False a smaller than p
+// "2" > "12" // True
+// undefined == null // True // null equal to undefined only
+// undefined === null // False
+// null == "\n0\n" // False
+// null === +"\n0\n" // False
